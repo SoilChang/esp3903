c14b4fb24ce8687a50953e96bb007070e3fd4139
charleschang <im741314@gmail.com>
2018-03-21 15:09:11 +0800

    Add All && Quick Commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-
-const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
